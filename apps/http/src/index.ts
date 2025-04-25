17582c865ea282fcd687876e03a45bedbf886ded
Aastha-Bharti <asbharti27@gmail.com>
2025-04-25 22:54:38 +0530

    new

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 
+
 app.get("/", (req: Request, res: Response) => {
   res.send("working");
 });
@@ -12,3 +13,5 @@ app.use("/auth", authRouter);
 app.listen(3000, () => {
   console.log("running on port 3000");
 });
+
+console.log("astha")
